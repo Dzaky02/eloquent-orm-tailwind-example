d358fe2efddf122bf931442a759131dde8643c39
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-05-25 15:46:40 +0700

    Adding navigation and image assets.

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -4,7 +4,7 @@
     
     <div style="font-family: 'Poppins', sans-serif">
         <div class="mx-auto flex flex-wrap flex-row items-center justify-between">
-            <a class="flex font-medium items-center" href="index.html">
+            <a class="flex font-medium items-center" href="{{ route('index') }}">
                 <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd"
                         d="M3.5 15.75C3.5 8.98451 8.98451 3.5 15.75 3.5H29.75C30.7165 3.5 31.5 4.2835 31.5 5.25C31.5 6.21649 30.7165 7 29.75 7H15.75C10.9175 7 7 10.9175 7 15.75V29.75C7 30.7165 6.2165 31.5 5.25 31.5C4.2835 31.5 3.5 30.7165 3.5 29.75V15.75Z"
@@ -29,10 +29,10 @@
                 id="menu-1-2">
                 <nav
                     class="lg:space-x-12 space-x-0 lg:flex items-center justify-between text-base pt-8 lg:pt-0 lg:space-y-0 space-y-6">
-                    <a class="block cursor-pointer nav-1-2" href="clubs.html">Clubs</a>
-                    <a class="block cursor-pointer nav-1-2" href="players.html">Players</a>
-                    <a class="block cursor-pointer nav-1-2" href="managers.html">Managers</a>
-                    <a class="block cursor-pointer nav-1-2" href="stadiums.html">Stadium</a>
+                    <a class="block cursor-pointer nav-1-2" href="{{ route('landing.clubs') }}">Clubs</a>
+                    <a class="block cursor-pointer nav-1-2" href="{{ route('landing.players') }}">Players</a>
+                    <a class="block cursor-pointer nav-1-2" href="{{ route('landing.managers') }}">Managers</a>
+                    <a class="block cursor-pointer nav-1-2" href="{{ route('landing.stadiums') }}">Stadium</a>
                 </nav>
             </div>
 
